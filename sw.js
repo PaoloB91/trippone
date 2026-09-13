b588343e61ba7e97120e3b85f66aa066bd61283b
PaoloB91 <paolo@pscompanysrl.com>
2026-09-13 12:51:47 +0200

    Aggiornamento Trippone 2026-09-13

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // dentro una versione non cambiano, e a ogni deploy la versione ruota e la cache riparte.
 // NON aggiornare CACHE_VERSION a mano: lo fa deploy.sh.
 
-const CACHE_VERSION = "trippone-v17";
+const CACHE_VERSION = "trippone-v18";
 const FILE = [
   "./",
   "./index.html",
